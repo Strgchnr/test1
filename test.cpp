b9a9fae9540320f89311ffc8608f99db6946216f
Cohinor <cohinor2@gmail.com>
2019-08-15 01:51:57 +0300

    tst

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <windows.h>
+#include <ctime>
 using namespace std;
 
 void SetColor(int text, int background)
@@ -9,30 +10,105 @@ void SetColor(int text, int background)
 }
 
 int main()
-{
-	int typeNumber ;
-	//programm body
+{   
+	int arraySize;
+	cout << ("Choose array size (number of cells)\n");
+	cin >> arraySize;
+	if (arraySize < 1)
+	{
+		SetColor(4, 0);
+		cout << ("err: Wrong format\n");
+		SetColor(15, 0);
+		system("pause");
+		return 0;
+	}
+	else 
+
+	system("cls");
+
+	int menuNumber ;
+	SetColor(9,0);
 	cout << ("Shoose sort type\n");
+	SetColor(15,0);
 	cout << ("----------------\n");
 	cout << ("1: Buble sort \n");
 	cout << ("2: Selection sort\n");
-	cout << ("3: insertion sort\n");
+	cout << ("3: Insertion sort\n");
 	cout << ("4: Merge sort\n");
-	cout << ("0: Exit\n");
-	cin >> typeNumber;
-	if (typeNumber >= 1)
+	cout << ("0:");
+	SetColor(4,0);
+	cout << (" Exit\n");
+	SetColor(15, 0);
+	cin >> menuNumber;
+    //проверить 
+	//srand(time(0));
+	//int *ptrarray= new int [arraySize];
+	//for (int count = 0; count < 10000; count++)
+		ptrarray[count] = (rand() % 1000 + 1) / int((rand() % 1000 + 1));
+
+
+	if (menuNumber >= 0)
 	{
 		//
-		cout << ("all ok");
-		system("pause");
+		switch (menuNumber)
+		{
+		case 1:
+			//buble sort 
+		/*	//проверить, настроить 
+		{
+			int tmp = 0;
+			for (int i = 0; i < lenD; i++) {
+				for (int j = (lenD - 1); j >= (i + 1); j--) {
+					if (data[j] < data[j - 1]) {
+						tmp = data[j];
+						data[j] = data[j - 1];
+						data[j - 1] = tmp;
+					}
+				}
+			}
+			cout что-то там, в общем тоже доделать
+		}*/
+			break;
+
+	    case 2:
+			//selektion sort
+			break;
+		case 3:
+			//insertion sort
+			break;
+		case 4:
+			//merge sort   
+			break;
+		case 0: 
+
+			return 0;
+		case 22:
+			//проверить 
+			system("cls");
+			for (int count = 0; count < arraySize; count++)
+				cout << ptrarray[count] << "    \n";
+			delete[] ptrarray; // высвобождение памяти
+			system("pause");
+			break;
+
+			break;
+
+
+		default:
+			SetColor(4,0);
+			cout << ("err: Unknown menu point\n");
+			SetColor(15, 0);
+			system("pause");
+			break;
+		}
 	}
 	else
-
-		SetColor(4,0);
+	 {
+		SetColor(4, 0);
 		cout << ("err: Wrong format \n");
-		SetColor(15,0);
+		SetColor(15, 0);
 		system("pause");
-		
+	 }
 		return 0;
 }
 
@@ -47,6 +123,8 @@ int main()
 //   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
 //   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
 /*
+22 in menu => test mode
+
 console colors
         Black         = 0,
 		Blue          = 1,
