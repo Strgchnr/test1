ec1577b6c2bf5611e289579575e01cc2afaabce8
Cohinor <cohinor2@gmail.com>
2019-08-15 01:13:51 +0300

    test commit

---
+++ test.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void SetColor(int text, int background)
+{
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hStdOut, (WORD)((background << 4) | text));
+}
+
+int main()
+{
+	int typeNumber ;
+	//programm body
+	cout << ("Shoose sort type\n");
+	cout << ("----------------\n");
+	cout << ("1: Buble sort \n");
+	cout << ("2: Selection sort\n");
+	cout << ("3: insertion sort\n");
+	cout << ("4: Merge sort\n");
+	cout << ("0: Exit\n");
+	cin >> typeNumber;
+	if (typeNumber >= 1)
+	{
+		//
+		cout << ("all ok");
+		system("pause");
+	}
+	else
+
+		SetColor(4,0);
+		cout << ("err: Wrong format \n");
+		SetColor(15,0);
+		system("pause");
+		
+		return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+/*
+console colors
+        Black         = 0,
+		Blue          = 1,
+		Green         = 2,
+		Cyan          = 3,
+		Red           = 4,
+		Magenta       = 5,
+		Brown         = 6,
+		LightGray     = 7,
+	   &nbsp;DarkGray = 8,
+		LightBlue     = 9,
+		LightGreen    = 10,
+		LightCyan     = 11,
+		LightRed      = 12,
+		LightMagenta  = 13,
+		Yellow        = 14,
+		White         = 15
+*/
