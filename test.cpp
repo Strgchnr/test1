888d819e27c4b4e246b4ce3d20935f137221971b
Cohinor <cohinor2@gmail.com>
2019-08-17 18:44:06 +0300

    Pre alfa

--- test.cpp
+++ test.cpp
@@ -31,6 +31,41 @@ void ArrPrint(int arr[], int size)
 		cout <<i<<(": ") <<arr[i] << "  \n";
 	cout << endl;
 }
+void SelectionSort(int arr[], int n)
+{
+	int i, j, min_idx;
+	// One by one move boundary of unsorted subarray  
+	for (i = 0; i < n - 1; i++)
+	{
+		// Find the minimum element in unsorted array  
+		min_idx = i;
+		for (j = i + 1; j < n; j++)
+			if (arr[j] < arr[min_idx])
+				min_idx = j;
+		// Swap the found minimum element with the first element  
+		swap(&arr[min_idx], &arr[i]);
+	}
+}
+void InsertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for (i = 1; i < n; i++)
+	{
+		key = arr[i];
+		j = i - 1;
+
+		/* Move elements of arr[0..i-1], that are
+		greater than key, to one position ahead
+		of their current position */
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+
 
 int main()
 {
@@ -69,7 +104,7 @@ int main()
 	cout << ("1: Buble sort \n");
 	cout << ("2: Selection sort\n");
 	cout << ("3: Insertion sort\n");
-	cout << ("4: Merge sort\n");
+	cout << ("4: Merge sort (will be finished at 0.2v)\n");
 	cout << ("0:");
 	SetColor(4,0);
 	cout << (" Exit\n");
@@ -90,14 +125,27 @@ int main()
 			cout << ("array was sorted \n");
 			ArrPrint(arr,ArrSize);
 			system("pause");
-			//cout
 			break;
-
 	    case 2:
 			//selektion sort
+			system("cls");
+			cout << ("sort started \n");
+			SelectionSort(arr, ArrSize);
+			//добавить проверку на правильность сортировки 
+			cout << ("array was sorted \n");
+			ArrPrint(arr, ArrSize);
+			system("pause");
 			break;
 		case 3:
 			//insertion sort
+			system("cls");
+			cout << ("sort started \n");
+			InsertionSort(arr, ArrSize);
+			//добавить проверку на правильность сортировки 
+			cout << ("array was sorted \n");
+			ArrPrint(arr, ArrSize);
+			system("pause");
+
 			break;
 		case 4:
 			//merge sort   
@@ -106,7 +154,7 @@ int main()
 
 			return 0;
 			break;
-		case 22://test mode 
+		case 616://test mode 
 			
 			system("cls");
 			cout << ("-=test mode=- \n");
@@ -139,18 +187,8 @@ int main()
 		return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
 /*
-22 in menu start test mode
+616 in menu start test mode
 >swap function description: 
 This use only in BubbleSort function.
 assigns the value "xp" to "yp" and "yp" to "xp".
